f8db0fc0fa9c0a6f5a9caea9caba1b429ff272a4
Muhammad Ahmad <93067257+MuhammadAhmadAk@users.noreply.github.com>
2022-12-19 22:28:17 +0500

    firebase

---
+++ lib/Controller/auth_controller.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:emart_app/consts/consts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthController extends GetxController {
+  var isCheck = false;
+  Future<UserCredential?> loginMethod({email, password, context}) async {
+    UserCredential? userCredential;
+    try {
+      userCredential = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      VxToast.show(context, msg: e.toString());
+    }
+    return userCredential;
+  }
+
+  Future<UserCredential?> signUpMethod({email, password, context}) async {
+    UserCredential? userCredential;
+    try {
+      userCredential = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      VxToast.show(context, msg: e.toString());
+    }
+    return userCredential;
+  }
+
+//store user Data
+  storeUserData({name, password, email}) async {
+    DocumentReference store =
+        firestore.collection(userCollection).doc(currentUser!.uid);
+    store.set(
+        {'name': name, 'passeword': password, 'email': email, 'imgUrl': ''});
+  }
+
+  signOutMethod(context) async {
+    try {
+      await auth.signOut();
+    } catch (e) {
+      VxToast.show(context, msg: e.toString());
+    }
+  }
+
+ 
+}
